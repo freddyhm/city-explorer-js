70a76e0bcaceecad92b7067e9db646fa648aa89c
Freddy Hidalgo-Monchez <freddyhm21@gmail.com>
2019-11-23 11:16:12 -0500

    change cypress test

--- cypress/integration/city-explorer-spec.js
+++ cypress/integration/city-explorer-spec.js
@@ -1,19 +1,19 @@
-describe('choosing an activity', function() {
-    it('shows one or more activities displayed in the home page', function(){
-        cy.visit('http://localhost:3000');
-        cy.get("[data-test='activities']")
-            .children()
-            .each(($el) => {
-                expect($el.text()).to.not.equal("");
-            });
-    });
-});
+// describe('choosing an activity', function() {
+//     it('shows one or more activities displayed in the home page', function(){
+//         cy.visit('http://localhost:3000');
+//         cy.get("[data-test='activities']")
+//             .children()
+//             .each(($el) => {
+//                 expect($el.text()).to.not.equal("");
+//             });
+//     });
+// });
 
 describe('choosing an activity type', function() {
 
     it('returns the places associated with that activity type', function() {
 
-        // cy.visit("http://localhost:3000");
+        cy.visit("/");
         //
         // cy.get('[data-test="activity-name"')
         //     .type('eating');
