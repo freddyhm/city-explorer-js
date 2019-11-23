76e8ab0f173d03cf68e1cfc0bb5de452f3122084
Freddy Hidalgo-Monchez <freddyhm21@gmail.com>
2019-11-23 08:58:57 -0500

    change mongoose uri variable to match heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
