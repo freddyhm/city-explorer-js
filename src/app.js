24b683861d79f5527cee86de4f7f482ac81e6864
Freddy Hidalgo-Monchez <freddyhm21@gmail.com>
2020-01-11 08:45:05 -0500

    remove model from app use since it is not middleware

--- src/app.js
+++ src/app.js
@@ -6,11 +6,9 @@ const app = express();
 
 const placeRouter = require('./routes/place');
 const activityRouter = require('./routes/activity');
-const category = require('./models/category');
 
 app.use(placeRouter);
 app.use(activityRouter);
-app.use(category);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
